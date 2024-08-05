c49346cf2b9f47fac1b1428b4dbbad4158a73a6c
md <27532803+gittyupcowboy@users.noreply.github.com>
2024-08-05 14:24:28 -0600

    Create qualys_config.py

---
+++ qualys_config.py
@@ -0,0 +1,9 @@
+# Example qualys_config.py
+# I might need to update the api_urls some day... I just keep adding to it... 
+
+# Qualys configuration variables
+QUALYS_API_URL = "https://qualysapi.qualys.com/qps/rest/2.0/"
+QUALYS_API_URL_V2 = "https://qualysapi.qualys.com/api/2.0/fo/asset/group/"
+QUALYS_API_URL_V3 = "https://qualysapi.qualys.com/api/2.0/fo/appliance/"
+QUALYS_USERNAME = ""
+QUALYS_PASSWORD = ""
