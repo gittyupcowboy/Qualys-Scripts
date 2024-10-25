f1d19083e5cfb62222afd4097cd48f1c460bdb46
md <27532803+gittyupcowboy@users.noreply.github.com>
2024-10-25 17:56:42 -0600

    Update qualys_config.py
    Need to refactor qualys_api_urls for consistency instead of adding new ones every time I forget to reference the existing ones... meh.

--- qualys_config.py
+++ qualys_config.py
@@ -5,5 +5,6 @@
 QUALYS_API_URL = "https://qualysapi.qualys.com/qps/rest/2.0/"
 QUALYS_API_URL_V2 = "https://qualysapi.qualys.com/api/2.0/fo/asset/group/"
 QUALYS_API_URL_V3 = "https://qualysapi.qualys.com/api/2.0/fo/appliance/"
+QUALYS_FO_API_URL = "https://qualysapi.qualys.com"
 QUALYS_USERNAME = ""
 QUALYS_PASSWORD = ""
